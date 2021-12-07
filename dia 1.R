1571493b25d2f4c52fb243a6509c5db8b7d1c561
Letisouza <leticiasouzaufpe@gmail.com>
2021-12-06 22:03:16 -0300

    Dia 1

---
+++ dia 1.R
@@ -0,0 +1,39 @@
+# O Ciclo da Ciência de Dados
+
+# Base da ciência de dados: vizualização, modelagem(regressão linear, logísitica, árvore de decisão e random forest),
+# 1 linguagem de programação e manipulação(SQL etc.)
+
+# Importar -> arrumar(armazenar os dados consistentemente) 
+# -> [transformar(criar novas variáveis e agregações) -> visualizar (surpreende, mas não é escalável) 
+#    -> modelar (é escalável, mas não surpreende)] 
+# -> comunicar e/ou automatizar
+
+# Engenheiros de dados: importação e manipulação
+# Analista de dados: manipulação e visualiação
+# Cientista de dados: modelagem
+
+# R
+# Importar(readr, xml2, haven, readxl), arrumar(tidyr, janitor, tibble, marittr), transformar(dplyr, stringr, forcats), 
+# visualizar(ggplot2), modelar(caret, mlr, lme4, keras), comunicar(rmarkdown, shiny),  automtizar(plumber, opencpu).
+
+##########################################################
+
+library(usethis)
+library(distill)
+
+# Criando um blog: o conteúdo segue na pasta Blog
+
+usethis::create_package("Blog") 
+
+
+# Vá para repositório "Blog"
+
+
+
+
+
+
+
+
+
+
